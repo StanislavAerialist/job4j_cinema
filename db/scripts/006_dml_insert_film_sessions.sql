e354ad39b2718479b313cb402c6fdae41a834257
StanislavAerialist <moonlight_stas@mail.ru>
2023-03-13 21:48:50 +0500

    Add FilmSession and models

---
+++ db/scripts/006_dml_insert_film_sessions.sql
@@ -0,0 +1,2 @@
+insert into film_sessions(film_id, halls_id, start_time, end_time, price) values(1, 1, timestamp '2023-03-15 15:00' , timestamp '2023-03-15 17:00', 300);
+insert into film_sessions(film_id, halls_id, start_time, end_time, price) values(2, 2, timestamp '2023-03-15 18:00' , timestamp '2023-03-15 21:00', 500);
