7efcbbc4cd89df17173ad2e031fdcee5a4457136
StanislavAerialist <moonlight_stas@mail.ru>
2023-03-14 21:25:57 +0500

    Add Main and UserRepository

---
+++ src/main/java/ru/job4j/cinema/repository/Sql2oUserRepository.java
@@ -0,0 +1,73 @@
+package ru.job4j.cinema.repository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import ru.job4j.cinema.model.User;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public class Sql2oUserRepository implements UserRepository {
+    private static final Logger LOG = LoggerFactory.getLogger(Sql2oUserRepository.class.getName());
+
+    private final Sql2o sql2o;
+
+    public Sql2oUserRepository(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public Optional<User> save(User user) {
+        Optional<User> rsl = Optional.empty();
+        try (var connection = sql2o.open()) {
+            var sql = """
+                      INSERT INTO users(full_name, email, password)
+                      VALUES (:full_name, :email, :password)
+                      """;
+            var query = connection.createQuery(sql, true)
+                    .addParameter("full_name", user.getFullName())
+                    .addParameter("email", user.getEmail())
+                    .addParameter("password", user.getPassword());
+            Connection generatedId;
+            try {
+                generatedId = query.executeUpdate();
+                user.setId(generatedId.getKey(Integer.class));
+                rsl = Optional.of(user);
+            } catch (IllegalArgumentException e) {
+                LOG.error("Exception when save user", e);
+            }
+            return rsl;
+        }
+    }
+
+    @Override
+    public Optional<User> findByEmailAndPassword(String email, String password) {
+        try (var connection = sql2o.open()) {
+            var query = connection.createQuery("SELECT * FROM users WHERE email = :email AND password  = :password");
+            query.addParameter("email", email);
+            query.addParameter("password", password);
+            var user = query.setColumnMappings(User.COLUMN_MAPPING).executeAndFetchFirst(User.class);
+            return Optional.ofNullable(user);
+        }
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        try (var connection = sql2o.open()) {
+            var query = connection.createQuery("DELETE FROM users WHERE id = :id");
+            query.addParameter("id", id);
+            var affectedRows = query.executeUpdate().getResult();
+            return affectedRows > 0;
+        }
+    }
+
+    @Override
+    public Collection<User> findAll() {
+        try (var connection = sql2o.open()) {
+            var query = connection.createQuery("SELECT * FROM users");
+            return query.setColumnMappings(User.COLUMN_MAPPING).executeAndFetch(User.class);
+        }
+    }
+}
